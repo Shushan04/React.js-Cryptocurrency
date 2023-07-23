03a3e271d3f056d47d04aa56369c9a76610f37bf
Shushan04 <ash118817@gmail.com>
2023-07-23 15:45:29 +0400

    add about contact and home functional

--- src/app.js
+++ src/app.js
@@ -1,11 +1,41 @@
 import Header from './components/header';
-import CryptoCurrencyList from './pages/cryptoCurrencyList'; //page ---> 
+import CryptoCurrencyList from './pages/cryptoCurrencyList';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function About() {
+    return (
+        <div>
+            About
+        </div>
+    )
+}
+
+function Contact() {
+    return (
+        <div>
+            Contact
+        </div>
+    )
+}
 
 function App() {
     return (
         <div>
             <Header />
-            <CryptoCurrencyList />
+            {/* <CryptoCurrencyList /> */}
+
+            <BrowserRouter>
+                <Link to="/">Home</Link>
+                <Link to="/about">about</Link>
+                <Link to="/contact">contact</Link>
+
+                <Routes>
+                    <Route path='/' element={<CryptoCurrencyList/>}/>
+                    <Route path='/about' element={<About />}/>
+                    <Route path='/contact' element={<Contact />}/>
+
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
